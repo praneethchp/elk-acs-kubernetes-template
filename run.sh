31301d915e0b8760404ba6060a3f148982889e15
Zhiyuan Zheng <zhzy0077@hotmail.com>
2017-07-03 02:55:10 +0000

    add portal of kube

--- run.sh
+++ run.sh
@@ -5,20 +5,12 @@ set -e
 MASTER_DNS=$1
 LOCATION=$2
 MASTER_USERNAME=$3
-BASED_PRIVATE_KEY=$4
-REGISTRY_NAME=$5
-REGISTRY_PASS=$6
-STORAGE_ACCOUNT=$7
-STORAGE_LOCATION=$8
-
-echo $1
-echo $2
-echo $3
-echo $4
-echo $5
-echo $6
-echo $7
-echo $8
+NGINX_PASSWORD=$4
+BASED_PRIVATE_KEY=$5
+REGISTRY_NAME=$6
+REGISTRY_PASS=$7
+STORAGE_ACCOUNT=$8
+STORAGE_LOCATION=$9
 
 export REGISTRY_URL=${REGISTRY_NAME}.azurecr.io
 export STORAGE_ACCOUNT=$7
@@ -35,7 +27,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
-sudo apt-get install -y unzip docker-ce
+sudo apt-get install -y unzip docker-ce nginx apache2-utils
 
 # install kubectl
 cd /tmp
@@ -48,7 +40,7 @@ echo "${BASED_PRIVATE_KEY}" | base64 -d | tee ${PRIVATE_KEY}
 chmod 400 ${PRIVATE_KEY}
 
 mkdir -p /root/.kube
-scp -o StrictHostKeyChecking=no -i ${PRIVATE_KEY} ${MASTER_USERNAME}@${MASTER_URL}:.kube/config $KUBECONFIG
+scp -o StrictHostKeyChecking=no -i ${PRIVATE_KEY} ${MASTER_USERNAME}@${MASTER_URL}:.kube/config ${KUBECONFIG}
 kubectl get nodes
 
 # install helm
@@ -60,7 +52,16 @@ REPO_URL='https://github.com/yaweiw/elk-acs-kubernetes-template/archive/develop.
 
 curl -LO ${REPO_URL}
 unzip -o develop.zip -d template
-cd template/elk-acs-kubernetes-template-develop/docker
+
+# expose kubectl proxy
+cd template/elk-acs-kubernetes-template-develop
+echo ${NGINX_PASSWORD} | htpasswd -c -i /etc/nginx/.htpasswd ${MASTER_USERNAME}
+cp nginx-site.conf /etc/nginx/sites-available/default
+nohup kubectl proxy --port=8080 &
+systemctl reload nginx
+
+# push image & helm install 
+cd docker
 bash push-images.sh ${REGISTRY_NAME} ${REGISTRY_PASS}
 cd ../helm-charts
 bash start-elk.sh ${REGISTRY_NAME} ${REGISTRY_PASS}
